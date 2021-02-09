d91157cfb358c3816f35ca15cf4f25cf42b85d89
rasmus petersen <rasm00f1@stud.kea.dk>
2021-02-09 16:15:42 +0100

    added option 4-5-6

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let firstLetter = "";
 let rest = "";
 let result = document.querySelector("#output_field");
 let firstSpace = "";
+let secondSpace = "";
+let inputStringLength = "";
 
 function initialize() {
   console.log("Initializing");
@@ -28,8 +30,14 @@ function generateOutput() {
     console.log("Option 3");
     option3();
   } else if (formatOption.value === "Option_4") {
+    console.log("Option 4");
+    option4();
   } else if (formatOption.value === "Option_5") {
+    console.log("Option 5");
+    option5();
   } else if (formatOption.value === "Option_6") {
+    console.log("Option 6");
+    option6();
   } else if (formatOption.value === "Option_7") {
   } else if (formatOption.value === "Option_8") {
   } else {
@@ -57,6 +65,27 @@ function option3() {
   displayOutput3();
 }
 
+function option4() {
+  firstSpace = inputString.indexOf(" ");
+  secondSpace = inputString.lastIndexOf(" ");
+
+  displayOutput4();
+}
+
+function option5() {
+  inputStringLength = inputString.length;
+
+  if (inputString.substring(inputStringLength - 4, inputStringLength) === ".jpg") {
+    result.innerHTML = "True";
+  } else if (inputString.substring(inputStringLength - 4, inputStringLength) === ".png") {
+    result.innerHTML = "True";
+  } else {
+    result.innerHTML = "False";
+  }
+}
+
+function option6() {}
+
 function displayOutput1() {
   result.innerHTML = `${firstLetter}` + `${rest}`;
 }
@@ -67,4 +96,8 @@ function displayOutput2() {
 
 function displayOutput3() {
   result.innerHTML = firstLetter;
+}
+
+function displayOutput4() {
+  result.innerHTML = inputString.substring(firstSpace + 1, secondSpace);
 }
